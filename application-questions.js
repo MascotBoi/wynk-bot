add9453850a3aaace4f70e9d0e7cf700582a34a6
MascotBoi <85682853+MascotBoi@users.noreply.github.com>
2021-06-10 17:24:28 +0530

    Add files via upload

---
+++ application-questions.js
@@ -0,0 +1,16 @@
+module.exports =  [
+	"What Are You Applying as? (Casual,Competitive,Trader)",
+	"Whats Your IGN?",
+	"What is your Discord name with ID?",
+	"What is your current in-game level?",
+	"What is your KDR?",
+	"What is your KPG?",
+	"If applying as a Trader, how much is your inventory worth? (Mention NO otherwise)",
+	"How many Nukes do you have?",
+	"Which region do you play in?",
+	"Mention your 1st, 2nd and 3rd best weapons in-game",
+	"How many kills, on average, do you get in a public game?",
+	"Have you played clan wars before?",
+	"If yes, provide screenshot of previous clan wars card (links only).",
+	"How many kills (minimum) can you get in 3 hours of Clan Wars? *"
+];
---
+++ strings.js
@@ -0,0 +1,20 @@
+module.exports = {
+	unknownCommand: "I could not run that command.",
+	notInGuild: "This command can only be used in a guild.",
+	notAuth: "This command can only be used by an admin.",
+	error: "I could not run that command, please contact your server administrator.",
+	adminRole: "Admin",
+	unknownRole: "This server is missing the admin role, please contact your server administrator.",
+	notSettingUpEnd: "You have not setup the form.",
+	setSubmissionsReply: "Form submissions will now be sent to you.",
+	setSubmissionsChannelReply: "Form submissions will now be posted in this channel.",
+	submissionsNotSet: "Submissions have not been set, please contact your server administrator",
+	applicationCancel: "Application canceled.",
+	applicationFormFalseCancel: "You have not started an application form yet.",
+	applicationSent: "Congratulations your application has been sent!",
+	formSetupInProgress: "Someone else is already configuring the form.",
+	newFormSetup: "The new form has been setup.",
+	formReceiveMessage: params => `${params.user} has submitted a form.`,
+	formApplyMessage: params => `Application commands: \`\`\`${params.botChar}cancel, ${params.botChar}redo\`\`\``,
+	formSetupMessage: params => `Enter questions and enter \`${params.botChar}endsetup\` when done.`
+}
